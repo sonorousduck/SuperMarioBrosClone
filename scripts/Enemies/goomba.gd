6aaa72a69f7d5512e9c8ea03ee61cd6d05460a3f
Ryan Anderson <sonorousduck@gmail.com>
2024-11-12 21:30:14 -0700

    Got jumping on heads to work and hitting boxes

--- scripts/Enemies/goomba.gd
+++ scripts/Enemies/goomba.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CharacterBody2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -6,21 +6,71 @@ extends Node2D
 # 	pass # Replace with function body.
 
 const SPEED: int = 60
-var direction: int = 1
+var direction: int = -1
+var isSquished: bool = false
+
 
 @onready var raycast_right = $RayCastRight
+@onready var raycast_top = $RayCastTop
 @onready var raycast_left = $RayCastLeft
 @onready var sprite = $Sprite2D
+@onready var animation_player = $AnimationPlayer
 
+
+func handle_animation_player():
+	animation_player.play("death")
+
+
+func _ready() -> void:
+	var squished = get_node("Area2D")
+	squished.connect("squished", handle_animation_player)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if raycast_right.is_colliding():
+		# var collider = raycast_right.get_collider()
+
+		# if collider and collider.collision_layer & (1 << 2) != 0:
 		direction = -1
 		sprite.flip_h = true
 	if raycast_left.is_colliding():
+		# var collider = raycast_left.get_collider()
+
+		# if collider and collider.collision_layer & (1 << 2) != 0:
+		# 	direction = -1
+		# 	sprite.flip_h = true
 		direction = 1
 		sprite.flip_h = false
 
+	# Handle that you jumped on the enemy's head
+	# if raycast_top.is_colliding():
+	# 	var collided_with = raycast_top.get_collider()
+
+	# 	if collided_with:
+	# 		if collided_with.has_method("bounce"):
+	# 			collided_with.bounce()
+	# 			isSquished = true
+		
 
 func _physics_process(delta: float) -> void:
-	position.x += direction * SPEED * delta 
+	if isSquished:
+		return
+
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	velocity.x = direction * SPEED
+	move_and_slide()
+
+	# for i in range(()):
+	# 	var collision = get_slide_collision(i)
+
+
+
+	# 	if (collision.has_method("bounce")):
+	# 		print(collision)
+
+	# 	if collision.get_normal().y > 0.7:
+	# 		if collision.has_method("bounce"):
+	# 			collision.bounce()
+	# 			
+
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,14 +1,40 @@
 extends CharacterBody2D
 
+class_name Player
 
 const SPEED = 130.0
-const JUMP_VELOCITY = -350.0
+const GRAVITY_MODIFIER = 2
+const JUMP_VELOCITY = -500.0 
+const BOUNCE_VELOCITY = -200.0 
+
+enum PlayerMode {
+	SMALL,
+	BIG,
+	SHOOTING
+}
+
+var player_mode = PlayerMode.SMALL
+
+func handle_movement_collision(collision: KinematicCollision2D):
+	if (collision.get_collider() is Block):
+		var collision_angle = rad_to_deg(collision.get_angle())
+		if roundf(collision_angle) == 180:
+			(collision.get_collider() as Block).bump(player_mode)
+		pass
+	
+	# if (collision.get_collider() is Pipe):
+	# 	pass
+
+# This function is used when the player kills an enemy
+func bounce():
+	velocity.y = BOUNCE_VELOCITY
+	# move_and_slide()
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * delta * GRAVITY_MODIFIER
 	
 
 	if Input.is_action_just_pressed("esc"):
@@ -19,6 +45,9 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y *= 0.5;
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
@@ -26,5 +55,12 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+
+	var collision = get_last_slide_collision()
+	if collision != null:
+		handle_movement_collision(collision)
+
+
 
 	move_and_slide()
